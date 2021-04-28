6c28aa5af90e226cafc342b44d562dd4c86e5b20
chill-phil <78041889+chill-phil@users.noreply.github.com>
2021-04-27 19:44:45 -0500

    create list item with appropriate values and append them to main task list

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,24 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function(event) {
     event.preventDefault();
 
+
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+
+    //create list item
     var listItemEl = document.createElement("li");
         listItemEl.className = "task-item";
-        listItemEl.textContent = "New task";
+
+    //create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+        taskInfoEl.className = "task-info";
+
+        //add HTML content to div
+        taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
         
-        tasksToDoEl.appendChild(listItemEl);
+    listItemEl.appendChild(taskInfoEl);
+    tasksToDoEl.appendChild(listItemEl);
 }
 
 
